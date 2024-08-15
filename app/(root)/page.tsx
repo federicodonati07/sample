243def094abbff5dd722828602f5a22788eca2cc
federicodonati07 <realeaquila.929@gmail.com>
2024-08-15 21:06:43 +0200

    home page, menu small device only, no responsive

---
+++ app/(root)/page.tsx
@@ -0,0 +1,63 @@
+'use client'
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { CiMenuBurger } from "react-icons/ci";
+import { IoCloseOutline } from "react-icons/io5";
+import Link from 'next/link';
+import { GoHome } from "react-icons/go";
+import { AiOutlineProduct } from "react-icons/ai";
+import { CiUser } from "react-icons/ci";
+
+const Page = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibilityMenu = () => {
+        setIsVisible(!isVisible);
+    };
+
+    return (
+        <>
+            <div className='flex flex-row justify-center items-center  text-white relative'>
+                <span className='m-2 text-2xl tracking-widest'>SAMPLE</span>
+                <div className="absolute top-0 right-0 m-4">
+                    <CiMenuBurger
+                        onClick={toggleVisibilityMenu}
+                        className={`text-2xl transition-opacity duration-300 ${isVisible ? 'opacity-0' : 'opacity-100'}`}
+                    />
+                </div>
+            </div>
+            <div 
+                onClick={toggleVisibilityMenu}
+                className={`overflow-hidden text-white border border-slate-50 rounded-lg absolute top-0 left-0 w-full transition-all duration-500 ease-in-out ${isVisible ? 'max-h-screen' : 'max-h-0'}`}>
+                <div className="flex flex-col items-center p-6 m-6 space-y-4 font-black">
+                    <IoCloseOutline
+                        onClick={toggleVisibilityMenu}
+                        className={`text-4xl transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
+                    />
+                    <Link href="/">
+                        <span className="hover:underline flex flex-row">
+                            <GoHome className='mr-2 text-2xl'/>Home
+                        </span>
+                    </Link>
+                    <Link href="/profile">
+                        <span className="hover:underline flex flex-row">
+                            <CiUser className='mr-2 text-2xl'/>Profile
+                        </span>
+                    </Link>
+                    <Link href="/collection">
+                        <span className="hover:underline flex flex-row">
+                            <AiOutlineProduct className='mr-2 text-2xl'/>Collection
+                        </span>
+                    </Link>
+                </div>
+            </div>
+
+            <div
+            className={`text-white flex flex-row m-20 transition-all duration-250 ease-in ${isVisible ? 'opacity-0' : 'opacity-100'}`}>
+                
+            </div>
+        </>
+    );
+};
+
+export default Page;
