049307aeb9e265ee5067e6c09f2a01ea3f7f781f
federicodonati07 <realeaquila.929@gmail.com>
2024-08-25 01:59:32 +0200

    toastNotification anche su editUser e editShipping

--- components/auth/private/editShippingInfo.tsx
+++ components/auth/private/editShippingInfo.tsx
@@ -5,6 +5,8 @@ import { isValidNumber, parsePhoneNumber } from 'libphonenumber-js';
 import { Button } from '@/components/ui/button';
 import { Select, SelectTrigger, SelectContent, SelectItem } from '@/components/ui/select';
 import supabase from '@/lib/supabase/supabaseClient';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const EditShippingInfo = ({
   country,
@@ -98,7 +100,9 @@ const EditShippingInfo = ({
             .eq("profile_uuid", uuid)
 
           if(error){
-            console.log(error)
+            toast.error(`Error: ${error.message}`)
+          }else{
+            toast.success("Shipping info edited successfully")
           }
         }
       } catch {
@@ -122,148 +126,151 @@ const EditShippingInfo = ({
   }, [address, house_number, apartament_number, postal_code, phone_number, more_info, country, state, city]);
 
   return (
-    <div className="lg:border lg:border-gray-300 lg:rounded-lg p-4 lg:max-w-3xl lg:mx-auto shadow-lg">
-      <div className="text-center mb-4">
-        <span className="text-2xl font-bold">Shipping Information</span>
-      </div>
-      <div className="space-y-4">
-        <div className="flex flex-col">
-          <label className="text-lg font-semibold mb-1">Country:</label>
-          <Select
-            value={inputCountryValue}
-            onValueChange={handleCountryChange}
-          >
-            <SelectTrigger className="w-full border border-gray-300 rounded-lg">
-              <span>{inputCountryValue !== 'unassigned' ? Country.getCountryByCode(inputCountryValue)?.name : 'Select Country'}</span>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="unassigned">Select Country</SelectItem>
-              {Country.getAllCountries().map((country) => (
-                <SelectItem key={country.isoCode} value={country.isoCode}>
-                  {country.name}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+    <>
+      <div className="lg:border lg:border-gray-300 lg:rounded-lg p-4 lg:max-w-3xl lg:mx-auto shadow-lg">
+        <div className="text-center mb-4">
+          <span className="text-2xl font-bold">Shipping Information</span>
         </div>
-        <div className="flex flex-col">
-          <label className="text-lg font-semibold mb-1">State:</label>
-          <Select
-            value={inputStateValue}
-            onValueChange={handleStateChange}
-            disabled={inputCountryValue === 'unassigned'}
-          >
-            <SelectTrigger className="w-full  border border-gray-300 rounded-lg">
-              <span>{inputStateValue !== 'unassigned' ? states.find(s => s.isoCode === inputStateValue)?.name : 'Select State'}</span>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="unassigned">Select State</SelectItem>
-              {states.map((state) => (
-                <SelectItem key={state.isoCode} value={state.isoCode}>
-                  {state.name}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="flex flex-col">
-          <label className="text-lg font-semibold mb-1">City:</label>
-          <Select
-            value={inputCityValue}
-            onValueChange={handleCityChange}
-            disabled={inputStateValue === 'unassigned'}
-          >
-            <SelectTrigger className="w-full border border-gray-300 rounded-lg">
-              <span>{inputCityValue !== 'unassigned' ? cities.find(c => c.name === inputCityValue)?.name : 'Select City'}</span>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="unassigned">Select City</SelectItem>
-              {cities.map((city) => (
-                <SelectItem key={city.id} value={city.name}>
-                  {city.name}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
+        <div className="space-y-4">
+          <div className="flex flex-col">
+            <label className="text-lg font-semibold mb-1">Country:</label>
+            <Select
+              value={inputCountryValue}
+              onValueChange={handleCountryChange}
+            >
+              <SelectTrigger className="w-full border border-gray-300 rounded-lg">
+                <span>{inputCountryValue !== 'unassigned' ? Country.getCountryByCode(inputCountryValue)?.name : 'Select Country'}</span>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="unassigned">Select Country</SelectItem>
+                {Country.getAllCountries().map((country) => (
+                  <SelectItem key={country.isoCode} value={country.isoCode}>
+                    {country.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-lg font-semibold mb-1">State:</label>
+            <Select
+              value={inputStateValue}
+              onValueChange={handleStateChange}
+              disabled={inputCountryValue === 'unassigned'}
+            >
+              <SelectTrigger className="w-full  border border-gray-300 rounded-lg">
+                <span>{inputStateValue !== 'unassigned' ? states.find(s => s.isoCode === inputStateValue)?.name : 'Select State'}</span>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="unassigned">Select State</SelectItem>
+                {states.map((state) => (
+                  <SelectItem key={state.isoCode} value={state.isoCode}>
+                    {state.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-lg font-semibold mb-1">City:</label>
+            <Select
+              value={inputCityValue}
+              onValueChange={handleCityChange}
+              disabled={inputStateValue === 'unassigned'}
+            >
+              <SelectTrigger className="w-full border border-gray-300 rounded-lg">
+                <span>{inputCityValue !== 'unassigned' ? cities.find(c => c.name === inputCityValue)?.name : 'Select City'}</span>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="unassigned">Select City</SelectItem>
+                {cities.map((city) => (
+                  <SelectItem key={city.id} value={city.name}>
+                    {city.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
 
-        <div className="grid grid-cols-2 gap-4">
-          <div className="flex flex-col">
-            <label className="text-lg font-semibold mb-1">Address:</label>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col">
+              <label className="text-lg font-semibold mb-1">Address:</label>
+              <Input
+                type="text"
+                value={inputAddressValue}
+                onChange={(e) => setInputAddressValue(e.target.value)}
+                placeholder="Insert a value"
+                className="border border-gray-300 rounded-lg p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-lg font-semibold mb-1">House N.:</label>
+              <Input
+                type="text"
+                value={inputHouseNumberValue}
+                onChange={(e) => setInputHouseNumberValue(e.target.value)}
+                placeholder="Insert a value"
+                className="border border-gray-300 rounded-lg p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-lg font-semibold mb-1">Apartament N.:</label>
+              <Input
+                type="text"
+                value={inputApartamentNumberValue}
+                onChange={(e) => setInputApartamentNumberValue(e.target.value)}
+                placeholder="Insert a value"
+                className="border border-gray-300 rounded-lg p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-lg font-semibold mb-1">Postal Code:</label>
+              <Input
+                type="text"
+                value={inputPostalCodeValue}
+                onChange={(e) => setInputPostalCodeValue(e.target.value)}
+                placeholder="Insert a value"
+                className="border border-gray-300 rounded-lg p-2"
+              />
+            </div>
+          </div>
+          
+          <div className="flex flex-col mb-4">
+            <label className="text-lg font-semibold mb-1">
+              {isInvalidNumber ? (
+                <span className="text-red-600">Invalid Phone Number</span>
+              ) : (
+                'Phone Number (with +):'
+              )}
+            </label>
             <Input
-              type="text"
-              value={inputAddressValue}
-              onChange={(e) => setInputAddressValue(e.target.value)}
+              type="tel"
+              value={inputPhoneNumberValue}
+              onChange={(e) => setInputPhoneNumberValue(e.target.value)}
               placeholder="Insert a value"
               className="border border-gray-300 rounded-lg p-2"
             />
           </div>
+          
           <div className="flex flex-col">
-            <label className="text-lg font-semibold mb-1">House N.:</label>
+            <label className="text-lg font-semibold mb-1">More Info:</label>
             <Input
               type="text"
-              value={inputHouseNumberValue}
-              onChange={(e) => setInputHouseNumberValue(e.target.value)}
-              placeholder="Insert a value"
-              className="border border-gray-300 rounded-lg p-2"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label className="text-lg font-semibold mb-1">Apartament N.:</label>
-            <Input
-              type="text"
-              value={inputApartamentNumberValue}
-              onChange={(e) => setInputApartamentNumberValue(e.target.value)}
-              placeholder="Insert a value"
-              className="border border-gray-300 rounded-lg p-2"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label className="text-lg font-semibold mb-1">Postal Code:</label>
-            <Input
-              type="text"
-              value={inputPostalCodeValue}
-              onChange={(e) => setInputPostalCodeValue(e.target.value)}
+              value={inputMoreInfoValue}
+              onChange={(e) => setInputMoreInfoValue(e.target.value)}
               placeholder="Insert a value"
               className="border border-gray-300 rounded-lg p-2"
             />
           </div>
         </div>
-        
-        <div className="flex flex-col mb-4">
-          <label className="text-lg font-semibold mb-1">
-            {isInvalidNumber ? (
-              <span className="text-red-600">Invalid Phone Number</span>
-            ) : (
-              'Phone Number (with +):'
-            )}
-          </label>
-          <Input
-            type="tel"
-            value={inputPhoneNumberValue}
-            onChange={(e) => setInputPhoneNumberValue(e.target.value)}
-            placeholder="Insert a value"
-            className="border border-gray-300 rounded-lg p-2"
-          />
-        </div>
-        
-        <div className="flex flex-col">
-          <label className="text-lg font-semibold mb-1">More Info:</label>
-          <Input
-            type="text"
-            value={inputMoreInfoValue}
-            onChange={(e) => setInputMoreInfoValue(e.target.value)}
-            placeholder="Insert a value"
-            className="border border-gray-300 rounded-lg p-2"
-          />
+        <div className="text-center mt-4">
+          <Button onClick={handleEdit} className="bg-slate-50 text-slate-950">
+            Edit Shipping Info
+          </Button>
         </div>
       </div>
-      <div className="text-center mt-4">
-        <Button onClick={handleEdit} className="bg-slate-50 text-slate-950">
-          Edit Shipping Info
-        </Button>
-      </div>
-    </div>
+    <ToastContainer />
+  </>
   );
 };
 
--- components/auth/private/userInfo.tsx
+++ components/auth/private/userInfo.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import supabase from '@/lib/supabase/supabaseClient';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const UserInfo = ({ name, surname, email, role, orders }) => {
   const [inputNameValue, setInputNameValue] = useState(name || '');
@@ -15,7 +17,7 @@ const UserInfo = ({ name, surname, email, role, orders }) => {
         .eq('email', email);
 
       if (error) {
-        window.location.href = '/private';
+        toast.error(`Error: ${error.message}`)
       }
 
       if (data?.length === 0) {
@@ -28,7 +30,7 @@ const UserInfo = ({ name, surname, email, role, orders }) => {
           }]);
 
         if (error) {
-          window.location.href = '/private';
+          toast.error(`Error: ${error.message}`)
         }
       } else {
         const { data, error } = await supabase
@@ -41,7 +43,9 @@ const UserInfo = ({ name, surname, email, role, orders }) => {
           .single();
 
         if (error) {
-          window.location.href = '/private';
+          toast.error(`Error: ${error.message}`)
+        }else{
+          toast.success("User info edited successfully")
         }
       }
     }
@@ -53,51 +57,54 @@ const UserInfo = ({ name, surname, email, role, orders }) => {
   }, [name, surname]);
 
   return (
-    <div className="lg:border lg:border-gray-300 lg:rounded-lg p-4 lg:max-w-3xl lg:mx-auto mt-2 shadow-lg">
-      <div className="text-center mb-4">
-        <span className="text-2xl font-bold">User Information</span>
+    <>
+      <div className="lg:border lg:border-gray-300 lg:rounded-lg p-4 lg:max-w-3xl lg:mx-auto mt-2 shadow-lg">
+        <div className="text-center mb-4">
+          <span className="text-2xl font-bold">User Information</span>
+        </div>
+        <div className="space-y-4">
+          <div className="grid grid-cols-2 gap-4 mb-4">
+            <div className="flex flex-col">
+              <label className="text-lg font-semibold mb-1">Name:</label>
+              <Input
+                type="text"
+                value={inputNameValue}
+                onChange={(e) => setInputNameValue(e.target.value)}
+                placeholder="Insert a value"
+                className="border border-gray-300 rounded-lg p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-lg font-semibold mb-1">Surname:</label>
+              <Input
+                type="text"
+                value={inputSurnameValue}
+                onChange={(e) => setInputSurnameValue(e.target.value)}
+                placeholder="Insert a value"
+                className="p-2"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mb-4">
+            <Button onClick={handleEdit} className="bg-slate-50 text-slate-950">
+              Edit User Info
+            </Button>
+          </div>
+          <div className="space-y-2">
+            <div>
+              <span className="text-lg font-semibold">Email:</span> <span className="font-semibold text-gray-500">{email}</span>
+            </div>
+            <div>
+              <span className="text-lg font-semibold">Role:</span> <span className={`font-semibold text-gray-500 ${role === 'admin' ? 'text-teal-600' : role === 'veteran' ? 'text-violet-600' : role === 'member' ? 'text-green-600' : role === 'banned' ? 'text-red-600' : ''}`}>{role}</span>
+            </div>
+            <div>
+              <span className="text-lg font-semibold">Orders:</span> <span className="font-semibold text-gray-500">{orders}</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="space-y-4">
-        <div className="grid grid-cols-2 gap-4 mb-4">
-          <div className="flex flex-col">
-            <label className="text-lg font-semibold mb-1">Name:</label>
-            <Input
-              type="text"
-              value={inputNameValue}
-              onChange={(e) => setInputNameValue(e.target.value)}
-              placeholder="Insert a value"
-              className="border border-gray-300 rounded-lg p-2"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label className="text-lg font-semibold mb-1">Surname:</label>
-            <Input
-              type="text"
-              value={inputSurnameValue}
-              onChange={(e) => setInputSurnameValue(e.target.value)}
-              placeholder="Insert a value"
-              className="p-2"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center mb-4">
-          <Button onClick={handleEdit} className="bg-slate-50 text-slate-950">
-            Edit User Info
-          </Button>
-        </div>
-        <div className="space-y-2">
-          <div>
-            <span className="text-lg font-semibold">Email:</span> <span className="font-semibold text-gray-500">{email}</span>
-          </div>
-          <div>
-            <span className="text-lg font-semibold">Role:</span> <span className={`font-semibold text-gray-500 ${role === 'admin' ? 'text-teal-600' : role === 'veteran' ? 'text-violet-600' : role === 'member' ? 'text-green-600' : role === 'banned' ? 'text-red-600' : ''}`}>{role}</span>
-          </div>
-          <div>
-            <span className="text-lg font-semibold">Orders:</span> <span className="font-semibold text-gray-500">{orders}</span>
-          </div>
-        </div>
-      </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 };
 
