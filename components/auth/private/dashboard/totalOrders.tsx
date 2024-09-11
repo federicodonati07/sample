dbe35ea0e024b71ba8cd967f03f30f391557cf65
federicodonati07 <realeaquila.929@gmail.com>
2024-09-11 18:15:09 +0200

    aggiornato il totalOrders.tsx, older newest funzionante, sort by email funzionante

--- components/auth/private/dashboard/totalOrders.tsx
+++ components/auth/private/dashboard/totalOrders.tsx
@@ -5,7 +5,10 @@ import { format, parseISO } from 'date-fns';
 import { it } from 'date-fns/locale';
 import { FaBox, FaCalendarAlt, FaExternalLinkAlt } from "react-icons/fa";
 import { IoPricetags } from "react-icons/io5";
-import { MdAlternateEmail, MdCircle } from "react-icons/md";
+import { MdAlternateEmail } from "react-icons/md";
+import { Button } from '@/components/ui/button';
+import { FiArchive } from "react-icons/fi";
+import { RiArchiveStackLine } from "react-icons/ri";
 
 interface Order {
     id: number;
@@ -16,22 +19,15 @@ interface Order {
     order_status: string;
     price: number;
     product_uuid: string;
+    is_archived: boolean;
 }
 
 const orderStatusColors = {
-    unread: 'bg-gradient-to-r from-orange-500 to-orange-600 text-white',
-    processing: 'bg-gradient-to-r from-blue-500 to-blue-600 text-white',
-    shipped: 'bg-gradient-to-r from-green-500 to-green-600 text-white',
-    delivered: 'bg-gradient-to-r from-gray-500 to-gray-600 text-white',
-    canceled: 'bg-gradient-to-r from-red-500 to-red-600 text-white',
-};
-
-const orderStatusPingColors = {
-    unread: 'text-orange-400',
-    processing: 'text-blue-400',
-    shipped: 'text-green-400',
-    delivered: 'text-gray-400',
-    canceled: 'text-red-400',
+    unread: 'bg-red-500 text-white',
+    processing: 'bg-blue-500 text-white',
+    shipped: 'bg-green-500 text-white',
+    delivered: 'bg-violet-500 text-white',
+    canceled: 'bg-red-500 text-white',
 };
 
 const TotalOrders = () => {
@@ -46,12 +42,13 @@ const TotalOrders = () => {
     const [postalCode, setPostalCode] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [moreInfo, setMoreInfo] = useState('');
-
-    const [sortKey, setSortKey] = useState('');
+    
+    const [sortKey, setSortKey] = useState('created_at');
     const [sortOrder, setSortOrder] = useState('asc');
     const [statusFilter, setStatusFilter] = useState('');
     const [dateFilter, setDateFilter] = useState('newest');
     const [searchQuery, setSearchQuery] = useState('');
+    const [showArchived, setShowArchived] = useState(false);
 
     const formatDateTime = (dateString: string) => {
         const date = parseISO(dateString);
@@ -98,18 +95,28 @@ const TotalOrders = () => {
         fetchAllOrders();
     }, []);
 
+    useEffect(() => {
+        if (dateFilter === 'newest') {
+            setSortOrder('desc');
+        } else {
+            setSortOrder('asc');
+        }
+    }, [dateFilter]);
+
     const handleSearchChange = (e) => {
         const query = e.target.value.toLowerCase();
-        setSortKey(query.includes('@') ? 'profile_email' : 'order_uuid');
         setSearchQuery(query);
+        setSortKey('order_uuid'); // Default sortKey for UUID
     };
 
     const filteredOrders = orders
         .filter((order) =>
             (statusFilter ? order.order_status === statusFilter : true) &&
-            (searchQuery
-                ? order[sortKey]?.toLowerCase().includes(searchQuery)
-                : true)
+            (searchQuery ? 
+                order.order_uuid.toLowerCase().includes(searchQuery) ||
+                order.profile_email.toLowerCase().includes(searchQuery) 
+                : true) &&
+            (!order.is_archived || showArchived)  // Mostra archiviati solo se showArchived è true
         );
 
     const sortedAndFilteredOrders = filteredOrders
@@ -119,10 +126,10 @@ const TotalOrders = () => {
                 const dateB = new Date(b[sortKey]);
 
                 if (isNaN(dateA.getTime()) || isNaN(dateB.getTime())) {
-                    return 0; // Se una delle date non è valida, le considera uguali
+                    return 0;
                 }
 
-                return dateFilter === 'newest'
+                return sortOrder === 'desc'
                     ? dateB.getTime() - dateA.getTime()
                     : dateA.getTime() - dateB.getTime();
             }
@@ -154,30 +161,37 @@ const TotalOrders = () => {
                                 <option value='delivered'>Delivered</option>
                                 <option value='canceled'>Canceled</option>
                             </select>
-                            <select
-                                className='p-2 border border-gray-600 rounded-lg bg-gray-800 text-white'
-                                onChange={(e) => setDateFilter(e.target.value)}
+
+                            <button
+                                className={`p-2 border ${dateFilter === 'newest' ? 'bg-blue-600' : 'bg-gray-800'} border-gray-600 rounded-lg text-white`}
+                                onClick={() => setDateFilter('newest')}
                             >
-                                <option value='newest'>Newest First</option>
-                                <option value='oldest'>Oldest First</option>
-                            </select>
+                                Newest First
+                            </button>
+                            <button
+                                className={`p-2 border ${dateFilter === 'oldest' ? 'bg-blue-600' : 'bg-gray-800'} border-gray-600 rounded-lg text-white`}
+                                onClick={() => setDateFilter('oldest')}
+                            >
+                                Oldest First
+                            </button>
+                            <Button
+                                className={`p-2 m-4 border ${showArchived ? 'bg-red-600' : 'bg-green-600'} border-gray-600 rounded-lg text-white`}
+                                onClick={() => setShowArchived(!showArchived)}
+                            >{showArchived ? <FiArchive className='text-xl'/> : <RiArchiveStackLine className='text-xl'/>} Archived</Button>
                         </div>
                     </div>
                 </div>
+
                 <div className='w-full max-w-4xl h-[400px] overflow-y-auto'>
                     {sortedAndFilteredOrders.map((order) => (
                         <React.Fragment key={order.id}>
                             <div className='flex flex-col border border-gray-700 rounded-xl bg-gray-800 text-white p-4 shadow-lg hover:shadow-xl transition-shadow duration-300 mb-4'>
                                 <div className='flex items-center justify-between mb-4'>
                                     <div className='flex items-center'>
-                                        <FaBox className='text-3xl text-yellow-400 mr-3' />
+                                                                                <FaBox className='text-3xl text-yellow-400 mr-3' />
                                         <span className='font-bold text-xl text-orange-500'>{order.order_uuid}</span>
                                         <div className='ml-4'>
                                             <div className={`flex items-center space-x-2 p-2 rounded-full ${orderStatusColors[order.order_status]} shadow-md`}>
-                                                <span className={`relative flex h-3 w-3 ${orderStatusPingColors[order.order_status]}`}>
-                                                    <span className={`absolute inline-flex h-full w-full rounded-full opacity-75 ${orderStatusPingColors[order.order_status]}`}></span>
-                                                    <MdCircle className={`animate-ping relative inline-flex h-3 w-3 rounded-full ${orderStatusPingColors[order.order_status]}`} />
-                                                </span>
                                                 <span className='text-md font-black capitalize'>{order.order_status}</span>
                                             </div>
                                         </div>
@@ -231,3 +245,4 @@ const TotalOrders = () => {
 }
 
 export default TotalOrders;
+
